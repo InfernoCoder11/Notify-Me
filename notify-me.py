f82b69e112be84dbbd7193f60d5aa04385ce6f6a
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-05-08 16:03:02 +0530

    Using argparser to parse arguments

--- notify-me.py
+++ notify-me.py
@@ -1,12 +1,12 @@
 import sys
 import subprocess as sub
+import argparse
 
-def ParseSysArgv():
-	args = sys.argv
-	args.pop(0)
-
-	return args
+parser = argparse.ArgumentParser()
+parser.add_argument("heading", help = "Heading of the notification", type = str)
+parser.add_argument("message", help = "Message body of the notification", type = str)
+parser.parse_args()
 
 if __name__ == "__main__":
 	#sub.call(["notify-send", sys.argv[1], sys.argv[2]])
-	print (ParseSysArgv())
+	pass
