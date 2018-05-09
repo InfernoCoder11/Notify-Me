4e9f892a32a9139b5ea0930d9f3adc79fbe87df1
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-05-09 21:16:11 +0530

    Added Notify Function

--- notify-me.py
+++ notify-me.py
@@ -1,12 +1,20 @@
 import sys
 import subprocess as sub
 import argparse
+import time
+import threading
 
 parser = argparse.ArgumentParser()
 parser.add_argument("heading", help = "Heading of the notification", type = str)
 parser.add_argument("message", help = "Message body of the notification", type = str)
-parser.parse_args()
+parser.add_argument("-m", "--minutes", help = "Notify after how many minutes", type = float)
+args = parser.parse_args()
+
+def Notify(heading, message, minutes = 0):
+	seconds = minutes*60
+	while seconds > 1:
+		pass
 
 if __name__ == "__main__":
-	#sub.call(["notify-send", sys.argv[1], sys.argv[2]])
+	sub.call(["notify-send", args.heading, args.message])
 	pass
